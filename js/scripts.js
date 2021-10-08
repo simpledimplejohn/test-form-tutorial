c41581f8a8af74c51659adb1603c06fbe2d4c825
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-08 14:27:25 -0700

    add madLibs.js jQuery form example

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
 //document.body.innerHTML = "works"
 //console.log("works")
 
-$(document).ready(function() {
-  $("button#first").click(function() {
-    $("body").addClass("green-background");
-  })
-})
