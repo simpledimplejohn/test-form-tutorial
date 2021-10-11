b48559da88d20ac8ddbd00d906c0ed5425f561a6
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-11 15:11:33 -0700

    add form call and console.log to radioButtons.js

--- js/radioButtons.js
+++ js/radioButtons.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
   $('#radio-form').submit(function(event) {
     event.preventDefault();
     const flavor = $("input:radio[name=flavor]:checked").val();
+    const color =$("input:radio[name=color]:checked").val();
+    const beatle =$("input:radio[name=beatle]:checked").val();
+
+
     console.log(flavor);
+    console.log(color);
+    console.log(beatle);
   })
 })
