9161745d67c4412ed1da92c78e84a9630526c3d8
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-11 17:44:46 -0700

    output rounded numbers to variables

--- js/radioButtons.js
+++ js/radioButtons.js
@@ -4,10 +4,25 @@ $(document).ready(function() {
     const q1 = $("input:radio[name=q1]:checked").val();
     const q2 =$("input:radio[name=q2]:checked").val();
     const q3 =$("input:radio[name=q3]:checked").val();
+    let answersArray = [q1,q2,q3]
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    for(let char of answersArray) {
+      if(char === "a") {
+        a ++
+      }
+      if(char === "b") {
+        b ++
+      }
+      if(char === "c") {
+        c ++
+      }
+    }
+    a = Math.round((a/8)*100)
+    b = Math.round((b/8)*100)
+    c = Math.round((c/8)*100)
+    console.log(a, b, c)
 
-
-    console.log(q1);
-    console.log(q2);
-    console.log(q3);
   })
 })
